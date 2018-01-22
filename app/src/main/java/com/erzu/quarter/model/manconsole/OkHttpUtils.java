670464b8680dbb5bf0d3ceedc6d3ee62275b8183
wxyxy <787738258@qq.com>
2018-01-22 15:08:32 +0800

    第一次提交

---
+++ app/src/main/java/com/erzu/quarter/model/manconsole/OkHttpUtils.java
@@ -0,0 +1,30 @@
+package com.erzu.quarter.model.manconsole;
+
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+
+
+/**
+ * Created by hp on 2018/1/22.
+ */
+
+public class OkHttpUtils {
+    private static OkHttpClient client = null;
+
+    public static OkHttpClient getInstance(){
+        if(client==null){
+            synchronized (OkHttpUtils.class){
+                if(client==null){
+                    client = new OkHttpClient.Builder()
+                            .connectTimeout(2000, TimeUnit.SECONDS)
+                            .writeTimeout(2000,TimeUnit.SECONDS)
+                            .readTimeout(2000,TimeUnit.SECONDS)
+                            .addInterceptor(new SongInterceptor())
+                            .build();
+                }
+            }
+        }
+        return client;
+    }
+}
---
+++ app/src/main/java/com/erzu/quarter/model/manconsole/RetrofitUtils.java
@@ -0,0 +1,32 @@
+package com.erzu.quarter.model.manconsole;
+
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.scalars.ScalarsConverterFactory;
+
+/**
+ * Created by hp on 2018/1/22.
+ */
+
+public class RetrofitUtils {
+    private static IGetService service = null;
+    //http://api.tianapi.com/nba/?key=71e58b5b2f930eaf1f937407acde08fe&num=10
+    //单例模式
+    public static IGetService getInstance(){
+        if(service==null){
+            synchronized (RetrofitUtils.class){
+                if(service==null){
+                    Retrofit retrofit = new Retrofit.Builder()
+                            .baseUrl("http://api.tianapi.com")
+                            .addConverterFactory(ScalarsConverterFactory.create())
+                            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                            .client(OkHttpUtils.getInstance())
+                            .build();
+
+                    service = retrofit.create(IGetService.class);
+                }
+            }
+        }
+        return service;
+    }
+}
