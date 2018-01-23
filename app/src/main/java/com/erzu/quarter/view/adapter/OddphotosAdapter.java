ff446904e63f2dc77ec0918953135a8b43f7b82a
xiaonaaa <1953597737@qq.com>
2018-01-23 21:21:34 +0800

    1

---
+++ app/src/main/java/com/erzu/quarter/view/adapter/OddphotosAdapter.java
@@ -0,0 +1,85 @@
+package com.erzu.quarter.view.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.TextView;
+
+import com.erzu.quarter.R;
+import com.erzu.quarter.model.bean.RecommendVideoBean;
+import com.facebook.drawee.view.SimpleDraweeView;
+
+import java.util.List;
+
+/**
+ * Created by lenovo on 2018/1/23.
+ */
+
+public class OddphotosAdapter extends RecyclerView.Adapter<OddphotosAdapter.MyHodler> {
+    List<RecommendVideoBean.DataBean> list;
+    Context context;
+
+    private View view;
+
+
+    public OddphotosAdapter(Context context, List<RecommendVideoBean.DataBean> data) {
+        this.context = context;
+        this.list = data;
+    }
+
+
+    @Override
+    public MyHodler onCreateViewHolder(ViewGroup parent, int viewType) {
+        view = View.inflate(context, R.layout.oddphotos_item, null);
+        MyHodler myHodler = new MyHodler(view);
+        return myHodler;
+    }
+
+    @Override
+    public void onBindViewHolder(MyHodler holder, int position) {
+
+
+        holder.mTitleText.setText(list.get(position).getUser().getNickname());
+        holder.mTimeText.setText(list.get(position).getCreateTime());
+        holder.mText01Qutu.setText(list.get(position).getWorkDesc());
+        holder.mText02Qutu.setText(list.get(position).getWorkDesc());
+//        holder.mTextQutu.setText(list.get(position).getComments().get(position).getContent());
+    }
+
+    @Override
+    public int getItemCount() {
+        return list == null ? 0 : list.size();
+    }
+
+    public class MyHodler extends RecyclerView.ViewHolder {
+        private SimpleDraweeView yuan;
+        private SimpleDraweeView mUserIamgeQutu;
+        private TextView mTitleText;
+        private TextView mTimeText;
+        private TextView mTextQutu;
+        private ImageView mMesgIamgeQutu;
+        private RadioButton mBtn2;
+        private RadioButton mBtn3;
+        private RadioButton mBtn4;
+        private RadioButton mBtn5;
+        private TextView mText01Qutu;
+        private TextView mText02Qutu;
+        public MyHodler(View itemView) {
+            super(itemView);
+            mUserIamgeQutu = (SimpleDraweeView) itemView.findViewById(R.id.qutu_user_iamge);
+            mTitleText = (TextView) itemView.findViewById(R.id.text_title);
+            mTimeText = (TextView) itemView.findViewById(R.id.text_time);
+            mTextQutu = (TextView) itemView.findViewById(R.id.qutu_text);
+            mMesgIamgeQutu = (ImageView) itemView.findViewById(R.id.qutu_mesg_iamge);
+            mBtn2 = (RadioButton) itemView.findViewById(R.id.btn2);
+            mBtn3 = (RadioButton) itemView.findViewById(R.id.btn3);
+            mBtn4 = (RadioButton) itemView.findViewById(R.id.btn4);
+            mBtn5 = (RadioButton) itemView.findViewById(R.id.btn5);
+            mText01Qutu = (TextView) itemView.findViewById(R.id.qutu_text01);
+            mText02Qutu = (TextView) itemView.findViewById(R.id.qutu_text02);
+        }
+    }
+}
--- app/src/main/java/com/erzu/quarter/view/fragment/OddphotosFragment.java
+++ app/src/main/java/com/erzu/quarter/view/fragment/OddphotosFragment.java
@@ -3,34 +3,71 @@ package com.erzu.quarter.view.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.erzu.quarter.ImagGithub.ImgGithub;
 import com.erzu.quarter.R;
-import com.facebook.drawee.view.SimpleDraweeView;
+import com.erzu.quarter.model.bean.RecommendBannerBean;
+import com.erzu.quarter.model.bean.RecommendVideoBean;
+import com.erzu.quarter.model.httpfz.APIFactory;
+import com.erzu.quarter.presenter.RecommendPresenter;
+import com.erzu.quarter.view.IView.IRecommendVeiw;
+import com.erzu.quarter.view.adapter.OddphotosAdapter;
+import com.erzu.quarter.view.adapter.RecommendAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 趣图页面
  * Created by hp on 2018/1/19.
  */
-public class OddphotosFragment extends Fragment {
-    private SimpleDraweeView mUserIamgeQutu;
-    private SimpleDraweeView mMesgIamgeQutu;
+public class OddphotosFragment extends Fragment implements IRecommendVeiw{
 
+    private RecyclerView mRyvOddphotos;
+    List<String> list_banner;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = View.inflate(getActivity(), R.layout.fragment_oddphotos, null);
-        mUserIamgeQutu = (SimpleDraweeView) view.findViewById(R.id.qutu_user_iamge);
-        mMesgIamgeQutu = (SimpleDraweeView) view.findViewById(R.id.qutu_mesg_iamge);
 
-        mUserIamgeQutu.setImageURI("http://tupian.qqjay.com/u/2013/1127/19_222949_14.jpg");
-        mMesgIamgeQutu.setImageURI("http://tupian.qqjay.com/u/2013/1127/19_222949_3.jpg");
+        mRyvOddphotos = (RecyclerView) view.findViewById(R.id.oddphotos_ryv);
+
+        RecommendPresenter presenter=new RecommendPresenter(this);
+        presenter.showRecomend();
+        presenter.showVideo();
+
         return view;
     }
 
+    @Override
+    public void onSuccess(RecommendBannerBean recommendBannerBean) {
+
+    }
+
+    @Override
+    public void onFailure(Exception e) {
+
+    }
 
 
+    @Override
+    public void Success_video(RecommendVideoBean recommendVideoBean) {
+        List<RecommendVideoBean.DataBean> data = recommendVideoBean.getData();
+        mRyvOddphotos.setLayoutManager(new LinearLayoutManager(getActivity()));
+        OddphotosAdapter recyclerView_adapter = new OddphotosAdapter(getActivity(), data);
+        mRyvOddphotos.setAdapter(recyclerView_adapter);
+
+        }
+
+    @Override
+    public void Failure(Exception e) {
+
+    }
 
 }
