b62284c0b00f58f12f4ca9565316e6f2d77d11e9
wxyxy <787738258@qq.com>
2018-01-25 20:21:03 +0800

    消息通知页面 提交2

--- app/src/main/java/com/erzu/quarter/view/fragment/MessageFragment.java
+++ app/src/main/java/com/erzu/quarter/view/fragment/MessageFragment.java
@@ -3,12 +3,14 @@ package com.erzu.quarter.view.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.erzu.quarter.R;
+import com.erzu.quarter.view.adapter.MessageFragmentAdapter;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -29,7 +31,9 @@ public class MessageFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = View.inflate(getActivity(), R.layout.fragment_message, null);
         unbinder = ButterKnife.bind(this, view);
-
+        messageRecyclerview.setLayoutManager(new LinearLayoutManager(getActivity()));
+        MessageFragmentAdapter adapter = new MessageFragmentAdapter(getActivity());
+        messageRecyclerview.setAdapter(adapter);
         return view;
     }
 
