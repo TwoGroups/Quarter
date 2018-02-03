4f795ed557a7dc4459618d60e9dd7e846e00c9b9
tiemaocsdn <tiemaocsdn@qq.com>
2018-02-03 13:56:39 +0800

    Merge branch 'master' of https://github.com/TwoGroups/Quarter
    # Conflicts:
    #	library/build/generated/mockable-android-17.v3.jar
    #	library/build/intermediates/blame/res/androidTest/debug/single/layout.json
    #	library/build/intermediates/incremental/debug-mergeJniLibs/merge-state
    #	library/build/intermediates/incremental/mergeDebugAndroidTestResources/compile-file-map.properties
    #	library/build/intermediates/incremental/mergeDebugAndroidTestResources/merger.xml
    #	library/build/intermediates/incremental/mergeDebugAssets/merger.xml
    #	library/build/intermediates/incremental/mergeDebugJniLibFolders/merger.xml
    #	library/build/intermediates/incremental/mergeDebugShaders/merger.xml
    #	library/build/intermediates/incremental/packageDebugResources/compile-file-map.properties
    #	library/build/intermediates/incremental/packageDebugResources/merger.xml
    #	library/build/outputs/logs/manifest-merger-debug-report.txt

--- app/src/main/java/com/erzu/quarter/view/activity/SetActivity.java
+++ app/src/main/java/com/erzu/quarter/view/activity/SetActivity.java
@@ -1,15 +1,43 @@
 package com.erzu.quarter.view.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.erzu.quarter.R;
+import com.erzu.quarter.utils.SharedPrefsUtil;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class SetActivity extends AppCompatActivity {
+
+    @BindView(R.id.message_back)
+    LinearLayout messageBack;
+    @BindView(R.id.vision_num)
+    ImageView visionNum;
+    @BindView(R.id.huancun)
+    ImageView huancun;
+    @BindView(R.id.edit_login)
+    TextView editLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.edit_login)
+    public void onViewClicked() {
+        SharedPrefsUtil.putValue(this, "Account", "phone", "");
+        SharedPrefsUtil.putValue(this, "Account", "password", "");
+        SharedPrefsUtil.putValue(this, "Account", "token", "");
+        SharedPrefsUtil.putValue(this, "Account", "nickName", "");
+        SharedPrefsUtil.putValue(this, "Account", "uid", "");
+        finish();
     }
 }
